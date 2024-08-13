46e06b096863547e5b320a0339bda19c8354742f
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-13 16:44:18 +0530

    Create Stripe Browser Instance

---
+++ utils/get-stripe.ts
@@ -0,0 +1,12 @@
+import {loadStripe, Stripe} from '@stripe/stripe-js';
+
+let stripePromise: Stripe | null;
+
+const getStripe = async () => {
+  if (!stripePromise) {
+    stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY!)
+  }
+  return stripePromise;
+}
+
+export default getStripe;
