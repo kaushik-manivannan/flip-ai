caf3fd3d2943aa8fe6ab6fc3b07ca889f9a0a715
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-16 02:40:07 +0530

    Display Hero and Pricing Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import getStripe from "@/utils/get-stripe";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
+      <Pricing />
     </main>
   );
 }
