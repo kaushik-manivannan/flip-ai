b78f7ed3daf8bbb07643a0b5ea1ce63b7b1e9d5a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-16 02:35:41 +0530

    Fix Firebase Window Error

--- firebase.tsx
+++ firebase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+const db = getFirestore(app);
+
+export { db };
