94a2985a0416d9126ab1fe213362eb2f21db0a43
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-17 02:55:20 +0530

    Add Loading Spinner in Create Page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -8,6 +8,7 @@ import { db } from "@/firebase";
 import NavBar from "@/components/NavBar";
 import TextInputForm from "@/components/TextInputForm";
 import FlashcardsList from "@/components/FlashcardsList";
+import { Loader2 } from "lucide-react";
 
 interface CreatePageProps {}
 
@@ -18,10 +19,12 @@ const CreatePage: FC<CreatePageProps> = () => {
   const [text, setText] = useState('');
   const [name, setName] = useState('');
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+    setIsLoading(true);
+    
     try {
       const response = await fetch('/api/generate', {
         method: 'POST',
@@ -31,6 +34,7 @@ const CreatePage: FC<CreatePageProps> = () => {
 
       const data = await response.json();
       setFlashcards(data);
+      setIsLoading(false);
       setFlipped(new Array(data.length).fill(false));
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
@@ -82,18 +86,37 @@ const CreatePage: FC<CreatePageProps> = () => {
   return (
     <main className="h-screen flex flex-col">
       <NavBar />
-      <div className="flex-grow overflow-auto">
-        <FlashcardsList 
+      <div className="flex-grow overflow-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-thumb-primary dark:scrollbar-track-primary-foreground">
+        {!isLoading && flashcards.length === 0 && (
+          <div className="flex flex-col items-center justify-center text-center p-7 h-[75vh]">
+            <p className="text-2xl sm:text-4xl font-bold text-gray-600 dark:text-primary leading-9 sm:leading-[3.2rem] mb-5 sm:mb-6">
+              Welcome to Flip AI! Ready to turn your ideas into powerful flashcards?
+            </p>
+            <p className="text-lg sm:3xl text-gray-600 dark:text-foreground leading-9 font-light">
+            Enter a topic below and let's start learning!
+            </p>
+          </div>
+        )}
+        {isLoading && (
+          <div className="flex items-center justify-center h-64">
+            <Loader2 className="h-8 w-8 animate-spin" />
+            <p className="ml-2">Generating {text} Flashcards...</p>
+          </div>
+        )}
+        {!isLoading && (
+          <FlashcardsList 
           flashcards={flashcards} 
           handleCardClick={handleCardClick} 
           flipped={flipped}
-          className="p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-max"
-        />
+
+          className="p-4 grid gap-10 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 auto-rows-max 2xl:grid-cols-4"
+          />
+        )}
       </div>
       <TextInputForm 
         setText={setText}
         handleSubmit={handleSubmit}
-        className="p-4" 
+        className="p-4"
         flashcards={flashcards}
         setName={setName}
         saveFlashcards={saveFlashcards}
