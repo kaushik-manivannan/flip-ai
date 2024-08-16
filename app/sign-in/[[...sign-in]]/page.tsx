2ce177e286acc8a52e1c706da37adfaaa9b6477a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-17 02:54:42 +0530

    Display Different Nav for Sign In and Sign Up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,8 +4,8 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-screen">
-      <NavBar />
-      <main className="flex flex-col items-center justify-between mt-3 sm:mt-10 lg:mt-24">
+      <NavBar showMainNav={false} />
+      <main className="flex flex-col items-center justify-between mt-10 lg:mt-24">
         <SignIn />
       </main>
     </div>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,11 @@
-import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="min-h-screen">
-      <NavBar />
-      <main className="flex flex-col items-center justify-between mt-3 sm:mt-10">
+      <NavBar showMainNav={false} />
+      <main className="flex flex-col items-center justify-between mt-10">
         <SignUp />
       </main>
     </div>
