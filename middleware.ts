5c42cdfed8b1186aee01d911a79d1275ccc5fe87
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-20 04:22:04 +0530

    Set up Protected Routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+const isPublicRoute = createRouteMatcher([
+  "/", 
+  "/sign-in(.*)", 
+  "/sign-up(.*)",
+  "/api/webhook/stripe"
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
